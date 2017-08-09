0ea324e70337fc12d3a99d124dca0047c3eec246
tahaiz00 <taha.subline.izmir@hotmail.com>
2017-08-09 16:21:05 +0300

    f_and_g, fdot_gdot ve rev_from_r0v0 algoritmaları matlaba taşındı
    kod hataları düzeltildi

--- Ornekler.m
+++ Ornekler.m
@@ -1,28 +1,39 @@
-% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
-% Example_3_05
 % ˜˜˜˜˜˜˜˜˜˜˜˜
 %
-% This program uses Algorithm 3.2 and the data of
-% Example 3.5 to solve Kepler’s equation for the hyperbola.
+% This program computes the state vector (R,V) from the
+% initial state vector (R0,V0) and the elapsed time using the
+% data in Example 3.7.
 %
-% e - eccentricity
-% M - hyperbolic mean anomaly (dimensionless)
-% F - hyperbolic eccentric anomaly (dimensionless)
+% mu - gravitational parameter (kmˆ3/sˆ2)
+% R0 - the initial position vector (km)
+% V0 - the initial velocity vector (km/s)
+% R - the final position vector (km)
+% V - the final velocity vector (km/s)
+% t - elapsed time (s)
 %
-% User M-function required: kepler_H
+% User M-functions required: rv_from_r0v0
 % ------------------------------------------------------------
 clear
-%...Input data for Example 3.5:
-e = 2.7696;
-M = 40.69;
+global mu
+mu = 398600;
+%...Input data for Example 3.7:
+R0 = [ 7000 -12124 0];
+V0 = [2.6679 4.6210 0];
+t = 3600;
 %...
-%...Pass the input data to the function kepler_H, which returns F:
-F = kepler_H(e, M);
-%...Echo the input data and output to the command window:
+%...Algorithm 3.4:
+[R, V] = rv_from_r0v0(R0, V0, t);
+%...Echo the input data and output the results to the command window:
 fprintf('---------------------------------------------------')
-fprintf('\n Example 3.5\n')
-fprintf('\n Eccentricity = %g',e)
-fprintf('\n Hyperbolic mean anomaly = %g\n',M)
-fprintf('\n Hyperbolic eccentric anomaly = %g',F)
+fprintf('\n Example 3.7\n')
+fprintf('\n Initial position vector (km):')
+fprintf('\n r0 = (%g, %g, %g)\n', R0(1), R0(2), R0(3))
+fprintf('\n Initial velocity vector (km/s):')
+fprintf('\n v0 = (%g, %g, %g)', V0(1), V0(2), V0(3))
+fprintf('\n\n Elapsed time = %g s\n',t)
+fprintf('\n Final position vector (km):')
+fprintf('\n r = (%g, %g, %g)\n', R(1), R(2), R(3))
+fprintf('\n Final velocity vector (km/s):')
+fprintf('\n v = (%g, %g, %g)', V(1), V(2), V(3))
 fprintf('\n-----------------------------------------------\n')
 % ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
---
+++ f_and_g.m
@@ -0,0 +1,23 @@
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+function [f, g] = f_and_g(x, t, ro, a)
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+%
+% This function calculates the Lagrange f and g coefficients.
+%
+% mu - the gravitational parameter (kmˆ3/sˆ2)
+% a - reciprocal of the semimajor axis (1/km)
+% ro - the radial position at time t (km)
+% t - the time elapsed since t (s)
+% x - the universal anomaly after time t (kmˆ0.5)
+% f - the Lagrange f coefficient (dimensionless)
+% g - the Lagrange g coefficient (s)
+%
+% User M-functions required: stumpC, stumpS
+% ------------------------------------------------------------
+global mu
+z = a*x^2;
+%...Equation 3.66a:
+f = 1 - x^2/ro*stumpC(z);
+%...Equation 3.66b:
+g = t - 1/sqrt(mu)*x^3*stumpS(z);
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
