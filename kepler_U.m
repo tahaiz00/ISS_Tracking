a91a6f996db8e14083f6bd5ce3d3caed2603a43d
tahaiz00 <taha.subline.izmir@hotmail.com>
2017-08-09 15:15:56 +0300

    StumpS, StumpC ve Kepler-U algoritmaları Matlab'te yazıldı
    algoritmalar matlab e aktarıldı.

---
+++ kepler_U.m
@@ -0,0 +1,50 @@
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+function x = kepler_U(dt, ro, vro, a)
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+%
+% This function uses Newton’s method to solve the universal
+% Kepler equation for the universal anomaly.
+%
+% mu - gravitational parameter (kmˆ3/sˆ2)
+% x - the universal anomaly (kmˆ0.5)
+% dt - time since x = 0 (s)
+% ro - radial position (km) when x = 0
+% vro - radial velocity (km/s) when x = 0
+% a - reciprocal of the semimajor axis (1/km)
+% z - auxiliary variable (z = a*xˆ2)
+% C - value of Stumpff function C(z)
+% S - value of Stumpff function S(z)
+% n - number of iterations for convergence
+% nMax - maximum allowable number of iterations
+%
+% User M-functions required: stumpC, stumpS
+% ------------------------------------------------------------
+global mu
+%...Set an error tolerance and a limit on the number of
+% iterations:
+error = 1.e-8;
+nMax = 1000;
+%...Starting value for x:
+x = sqrt(mu)*abs(a)*dt;
+%...Iterate on Equation 3.62 until convergence occurs within
+%...the error tolerance:
+n =0;
+ratio = 1;
+while abs(ratio) > error & n <= nMax
+n = n + 1;
+C = stumpC(a*xˆ2);
+S = stumpS(a*xˆ2);
+F = ro*vro/sqrt(mu)*xˆ2*C + (1 - a*ro)*xˆ3*S + ro*x-...
+sqrt(mu)*dt;
+dFdx = ro*vro/sqrt(mu)*x*(1 - a*xˆ2*S)+...
+(1 - a*ro)*xˆ2*C+ro;
+ratio = F/dFdx;
+x = x - ratio;
+end
+%...Deliver a value for x, but report that nMax was reached:
+if n > nMax
+fprintf('\n **No. iterations of Kepler''s equation')
+fprintf(' = %g', n)
+fprintf('\n F/dFdx = %g\n', F/dFdx)
+end
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
