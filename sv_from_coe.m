cf9b0c4eeccbd27a57b505002a2b80fed724a302
tahaiz00 <taha.subline.izmir@hotmail.com>
2017-08-10 12:05:10 +0300

    coe_from_coe coz

--- sv_from_coe.m
+++ sv_from_coe.m
@@ -38,17 +38,13 @@ TA = coe(6);
 rp = (h^2/mu) * (1/(1 + e*cos(TA))) * (cos(TA)*[1;0;0]+ sin(TA)*[0;1;0]);
 vp = (mu/h) * (-sin(TA)*[1;0;0] + (e + cos(TA))*[0;1;0]);
 %...Equation 4.39:
-R3_W = [ cos(RA) sin(RA) 0
--sin(RA) cos(RA) 0
-0 0 1];
+R3_W = [ cos(RA) sin(RA) 0-sin(RA) cos(RA) 0 0 0 1];
 %...Equation 4.40:
-R1_i = [1 0 0
-0 cos(incl) sin(incl)
-0 -sin(incl) cos(incl)];
+R1_i = [1 0 0 0 cos(incl) sin(incl) 0 -sin(incl) cos(incl)];
 %...Equation 4.41:
 R3_w = [ cos(w) sin(w) 0-sin(w) cos(w) 0 0 0 1];
 %...Equation 4.44:
-Q_pX = R3_W'*R1_i'*R3_w';
+Q_pX = R3_W' * R1_i' * R3_w';
 %...Equations 4.46 (r and v are column vectors):
 r = Q_pX*rp;
 v = Q_pX*vp;
