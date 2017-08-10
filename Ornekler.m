176cae9981f594f09726c739f815b2b6f4b5f452
tahaiz00 <taha.subline.izmir@hotmail.com>
2017-08-10 17:30:27 +0300

    syntax hataları cozuldu

--- Ornekler.m
+++ Ornekler.m
@@ -1,39 +1,46 @@
+% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+% Example_5_04
 % ˜˜˜˜˜˜˜˜˜˜˜˜
 %
-% This program computes the state vector (R,V) from the
-% initial state vector (R0,V0) and the elapsed time using the
-% data in Example 3.7.
+% This program computes J0 and the Julian day number using the
+% data in Example 5.4.
 %
-% mu - gravitational parameter (kmˆ3/sˆ2)
-% R0 - the initial position vector (km)
-% V0 - the initial velocity vector (km/s)
-% R - the final position vector (km)
-% V - the final velocity vector (km/s)
-% t - elapsed time (s)
+% year - range: 1901 - 2099
+% month - range: 1 - 12
+% day - range: 1 - 31
+% hour - range: 0 - 23 (Universal Time)
+% minute - range: 0 - 60
+% second - range: 0 - 60
+% ut - universal time (hr)
+% j0 - Julian day number at 0 hr UT
+% jd - Julian day number at specified UT
 %
-% User M-functions required: rv_from_r0v0
+% User M-function required: J0
 % ------------------------------------------------------------
 clear
-global mu
-mu = 398600;
-%...Input data for Example 3,7:
-R0 = [ 7000 -12124 0];
-V0 = [2.6679 4.6210 0];
-t = 3600;
+%...Input data from Example 5.4:
+year = 2004;
+month = 5;
+day = 12;
+hour = 14;
+minute = 45;
+second = 30;
 %...
-%...Algorithm 3.4:
-[R, V] = rv_from_r0v0(R0, V0, t);
+ut = hour + minute/60 + second/3600;
+%...Equation 5.48:
+j0 = J0(year, month, day);
+%...Equation 5.47:
+jd = j0 + ut/24;
 %...Echo the input data and output the results to the command window:
 fprintf('---------------------------------------------------')
-fprintf('\n Example 3.7\n')
-fprintf('\n Initial position vector (km):')
-fprintf('\n r0 = (%g, %g, %g)\n', R0(1), R0(2), R0(3))
-fprintf('\n Initial velocity vector (km/s):')
-fprintf('\n v0 = (%g, %g, %g)', V0(1), V0(2), V0(3))
-fprintf('\n\n Elapsed time = %g s\n',t)
-fprintf('\n Final position vector (km):')
-fprintf('\n r = (%g, %g, %g)\n', R(1), R(2), R(3))
-fprintf('\n Final velocity vector (km/s):')
-fprintf('\n v = (%g, %g, %g)', V(1), V(2), V(3))
+fprintf('\n Example 5.4: Julian day calculation\n')
+fprintf('\n Input data:\n');
+fprintf('\n Year = %g', year)
+fprintf('\n Month = %g', month)
+fprintf('\n Day = %g', day)
+fprintf('\n Hour = %g', hour)
+fprintf('\n Minute = %g', minute)
+fprintf('\n Second = %g\n', second)
+fprintf('\n Julian day number = %11.3f', jd);
 fprintf('\n-----------------------------------------------\n')
 % ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
