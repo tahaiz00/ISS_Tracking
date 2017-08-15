6908ef50799fa74cdddb2456a2adcb282b64bfea
tahaiz00 <taha.subline.izmir@hotmail.com>
2017-08-15 12:12:22 +0300

    gauss syntax hatalar coz

--- Ornekler.m
+++ Ornekler.m
@@ -1,118 +1,90 @@
-% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
-% Example_5_10
-% ˜˜˜˜˜˜˜˜˜˜˜˜
-%
-% This program uses Algorithms 5.4 and 4.1 to obtain the
-% orbital elements from the observational data provided in
-% Example 5.10.
-%
-% deg - conversion factor between degrees and radians
-% pi - 3.1415926...
-% mu - gravitational parameter (kmˆ3/sˆ2)
-%
-% Re - equatorial radius of the earth (km)
-% f - earth’s flattening factor
-% wE - angular velocity of the earth (rad/s)
-% omega - earth’s angular velocity vector (rad/s) in the
-% geocentric equatorial frame
-%
-% rho - slant range of object (km)
-% rhodot - range rate (km/s)
-% A - azimuth (deg) of object relative to observation
-% site
-% Adot - time rate of change of azimuth (deg/s)
-% a - elevation angle (deg) of object relative to
-% observation site
-% adot - time rate of change of elevation angle
-% (degrees/s)
-%
-% theta - local sidereal time (deg) of tracking site
-% phi - geodetic latitude (deg) of site
-% H - elevation of site (km)
-%
-% r - geocentric equatorial position vector of object (km)
-% v - geocentric equatorial velocity vector of object (km)
-%
-% coe - orbital elements [h e RA incl w TA a]
-% where h = angular momentum (kmˆ2/s)
-% e = eccentricity
-% RA = right ascension of the ascending node
-% (rad)
-% incl = inclination of the orbit (rad)
-% w = argument of perigee (rad)
-% TA = true anomaly (rad)
-% a = semimajor axis (km)
-% rp - perigee radius (km)
-% T - period of elliptical orbit (s)
-%
-% User M-functions required: rv_from_observe, coe_from_sv
-% ------------------------------------------------------------
 clear all
 clc
-
-global f Re wE mu
+global mu
 deg = pi/180;
-f = 1/298.256421867;
-Re = 6378.13655;
-wE = 7.292115e-5;
-mu = 398600.4418;
-%...Input data for Example 5.10:
-rho = 2551;
-rhodot = 0;
-A = 90;
-Adot = 0.1130;
-a = 30;
-adot = 0.05651;
-theta = 300;
-phi = 60;
-H = 0;
+mu = 398600;
+Re = 6378;
+f = 1/298.26;
+%...Input data:
+H =1;
+phi = 40*deg;
+t = [ 0 118.104 237.577];
+ra = [ 43.5365 54.4196 64.3178]*deg;
+dec = [-8.78334 -12.0739 -15.1054]*deg;
+theta = [ 44.5065 45.000 45.4992]*deg;
 %...
-%...Algorithm 5.4:
-[r,v] = rv_from_observe(rho, rhodot, A, Adot, a, adot, theta, phi, H);
-%...Algorithm 4.1:
+%...Equations 5.56 and 5.57:
+fac1 = Re/sqrt(1-(2*f - f*f)*sin(phi)^32);
+fac2 = (Re*(1-f)^2/sqrt(1-(2*f - f*f)*sin(phi)^2) + H)*sin(phi);
+for i = 1:3
+R(i,1) = (fac1 + H)*cos(phi)*cos(theta(i));
+R(i,2) = (fac1 + H)*cos(phi)*sin(theta(i));
+R(i,3) = fac2;
+rho(i,1) = cos(dec(i))*cos(ra(i));
+rho(i,2) = cos(dec(i))*sin(ra(i));
+rho(i,3) = sin(dec(i));
+end
+%...Algorithms 5.5 and 5.6:
+[r, v, r_old, v_old] = gauss(rho(1,:), rho(2,:), rho(3,:), R(1,:), R(2,:), R(3,:),t(1), t(2), t(3));
+%...Algorithm 4.1 for the initial estimate of the state vector
+% and for the iteratively improved one
+coe_old = coe_from_sv(r_old,v_old);
 coe = coe_from_sv(r,v);
-h = coe(1);
-e = coe(2);
-RA = coe(3);
-incl = coe(4);
-w = coe(5);
-TA = coe(6);
-a = coe(7);
-%...Equation 2.40
-rp = h^2/mu/(1 + e);
 %...Echo the input data and output the solution to
 % the command window:
 fprintf('---------------------------------------------------')
-fprintf('\n Example 5.10')
-fprintf('\n\n Input data:\n')
-fprintf('\n Slant range (km) = %g', rho)
-fprintf('\n Slant range rate (km/s) = %g', rhodot)
-fprintf('\n Azimuth (deg) = %g', A)
-fprintf('\n Azimuth rate (deg/s) = %g', Adot)
-fprintf('\n Elevation (deg) = %g', a)
-fprintf('\n Elevation rate (deg/s) = %g', adot)
-fprintf('\n Local sidereal time (deg) = %g', theta)
-fprintf('\n Latitude (deg) = %g', phi)
-fprintf('\n Altitude above sea level (km) = %g', H)
-fprintf('\n\n')
-fprintf(' Solution:')
-fprintf('\n\n State vector:\n')
-fprintf('\n r (km) = [%g, %g, %g]', ...
-r(1), r(2), r(3))
-fprintf('\n v (km/s) = [%g, %g, %g]', ...
-v(1), v(2), v(3))
-fprintf('\n\n Orbital elements:\n')
-fprintf('\n Angular momentum (kmˆ2/s) = %g', h)
-fprintf('\n Eccentricity = %g', e)
-fprintf('\n Inclination (deg) = %g', incl/deg)
-fprintf('\n RA of ascending node (deg) = %g', RA/deg)
-fprintf('\n Argument of perigee (deg) = %g', w/deg)
-fprintf('\n True anomaly (deg) = %g\n', TA/deg)
-fprintf('\n Semimajor axis (km) = %g', a)
-fprintf('\n Perigee radius (km) = %g', rp)
-%...If the orbit is an ellipse, output its period:
-if e < 1
-T = 2*pi/sqrt(mu)*a^1.5;
+fprintf('\n Example 5.11: Orbit determination by the Gauss method\n')
+fprintf('\n Radius of earth (km) = %g', Re)
+fprintf('\n Flattening factor = %g', f)
+fprintf('\n Gravitational parameter (kmˆ3/sˆ2) = %g', mu)
+fprintf('\n\n Input data:\n');
+fprintf('\n Latitude (deg) = %g', phi/deg);
+fprintf('\n Altitude above sea level (km) = %g', H);
+fprintf('\n\n Observations:')
+fprintf('\n Time (s) Right ascension (deg) Declination(deg)')
+fprintf(' Local sidereal time (deg)')
+for i = 1:3
+fprintf('\n %9.4g %17.4f %19.4f %23.4f',t(i), ra(i)/deg, dec(i)/deg, theta(i)/deg)
+end
+fprintf('\n\n Solution:\n')
+fprintf('\n Without iterative improvement...\n')
+fprintf('\n');
+fprintf('\n r (km) = [%g, %g, %g]',r_old(1), r_old(2), r_old(3))
+fprintf('\n v (km/s) = [%g, %g, %g]',v_old(1), v_old(2), v_old(3))
+fprintf('\n');
+fprintf('\n Angular momentum (kmˆ2/s) = %g', coe_old(1))
+fprintf('\n Eccentricity = %g', coe_old(2))
+fprintf('\n RA of ascending node (deg) = %g', coe_old(3)/deg)
+fprintf('\n Inclination (deg) = %g', coe_old(4)/deg)
+fprintf('\n Argument of perigee (deg) = %g', coe_old(5)/deg)
+fprintf('\n True anomaly (deg) = %g', coe_old(6)/deg)
+fprintf('\n Semimajor axis (km) = %g', coe_old(7))
+fprintf('\n Periapse radius (km) = %g', coe_old(1)^2/mu/(1 + coe_old(2)))
+%...If the orbit is an ellipse, output the period:
+if coe_old(2)<1
+T = 2*pi/sqrt(mu)*coe_old(7)^1.5;
+fprintf('\n Period:')
+fprintf('\n Seconds = %g', T)
+fprintf('\n Minutes = %g', T/60)
+fprintf('\n Hours = %g', T/3600)
+fprintf('\n Days = %g', T/24/3600)
+end
+fprintf('\n\n With iterative improvement...\n')
+fprintf('\n');
+fprintf('\n r (km) = [%g, %g, %g]', r(1), r(2), r(3))
+fprintf('\n v (km/s) = [%g, %g, %g]',v(1), v(2), v(3))
+fprintf('\n');
+fprintf('\n Angular momentum (kmˆ2/s) = %g', coe(1))
+fprintf('\n Eccentricity = %g', coe(2))
+fprintf('\n RA of ascending node (deg) = %g', coe(3)/deg)
+fprintf('\n Inclination (deg) = %g', coe(4)/deg)
+fprintf('\n Argument of perigee (deg) = %g', coe(5)/deg)
+fprintf('\n True anomaly (deg) = %g', coe(6)/deg)
+fprintf('\n Semimajor axis (km) = %g', coe(7))
+fprintf('\n Periapse radius (km) = %g', coe(1)^2/mu/(1 + coe(2)))
+%...If the orbit is an ellipse, output the period:
+if coe(2)<1
+T = 2*pi/sqrt(mu)*coe(7)^1.5;
 fprintf('\n Period:')
 fprintf('\n Seconds = %g', T)
 fprintf('\n Minutes = %g', T/60)
@@ -120,4 +92,4 @@ fprintf('\n Hours = %g', T/3600)
 fprintf('\n Days = %g', T/24/3600)
 end
 fprintf('\n-----------------------------------------------\n')
-% ˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜˜
+% ˜˜
